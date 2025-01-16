02ca2ab9ea7a01fa4f8ec44e18ce2f3340183de5
AnastasiiaMamaeva <mamaeva.anastas@gmail.com>
2025-01-16 13:54:03 +0200

    commit

--- seq_pars.py
+++ seq_pars.py
@@ -1,9 +1,11 @@
 import os
 import sys
 import re
+import tkinter as tk
+from tkinter import filedialog
 from Bio import Entrez
 
-# Set email for Entrez
+
 Entrez.email = "mamaeva.anastas@gmail.com"
 
 def is_ncbi_accession(text):
@@ -52,12 +54,74 @@ def save_FASTA(FASTA, folder_path, name):
     except Exception as e:
         print(f"Error saving FASTA data: {e}")
 
-if __name__ == "__main__":
-    if len(sys.argv) < 4:
-        print("Usage: python script.py <file_path> <db_name> <split/one>")
-        sys.exit(1)
+#a class to get file from tkinter
+class g_file_store():
+    def __init__(self):
+        self.f = "" #variable to store file path to a txt file with seq accessions
+        self.t = "" #variable to store the text that will be used to perse seq
+        self.db = "" #variable to store data base 
+        self.sl = "" #variable to store wether user wants one file or separat
+    #function to get the path to the file with sequences and save it in self.f
+    def g_file(self):
+        self.f = filedialog.askopenfilenames(title="Select Text Files", filetypes=[("Text files", "*.txt")])
+        if self.f:
+            lbl_2 = tk.Label(root, text="The File is located at : " + str(self.f), font=('Aerial 11'))
+            lbl_2.grid(row = 2, column = 0)
+    
+    #function to get keybord or file input into text as self.t
+    def r_text(self):
+        line = ""
+        line = entry.get()
+        if line:
+            self.t = line
 
-    file_path = sys.argv[1]
+        elif self.f:
+            with open(self.f, "r") as file:
+                self.t = file.read()
+        else:
+            lbl_3 = tk.Label(root, text="please input file or text", font=('Aerial 11'))
+            lbl_3.grid(row = 5, column = 0)
+        self.db = opt_db.get()
+        self.sl = opt_sl.get()
+
+
+
+
+if __name__ == "__main__":
+    
+    #create a storage for file
+    
+    
+    #create the main tk interface
+    
+    root = tk.Tk()
+    root.title("sequence parser")
+    new_path = g_file_store()
+
+
+    lbl = tk.Label(root, text="Select files containing sequences to parse:")
+    lbl.grid(row=0, column=0)
+
+    open_button = tk.Button(root, text="Open Files", command=new_path.g_file)
+    open_button.grid(row=0, column=0)
+
+    entry = tk.Entry(root)
+    entry.grid(row=3, column=0)
+    
+
+    opt_db_all = ["protein", "nucleotide"]
+    opt_db = tk.OptionMenu(root, tk.StringVar().set("protein"), *opt_db_all)
+    opt_db.grid(row =1, column = 1 )
+
+    opt_sl_all = ["one", "split"]
+    opt_sl = tk.OptionMenu(root, tk.StringVar().set("one"), *opt_sl_all)
+    opt_sl.grid(row = 2, column = 1)
+    submit_button = tk.Button(root, text="Submit", command=new_path.r_text)
+    submit_button.grid(row=4, column=0)
+
+    root.mainloop()
+
+
     db = sys.argv[2]
     fnum = sys.argv[3]
 
@@ -65,15 +129,6 @@ if __name__ == "__main__":
         print("Error: <split/one> argument must be 'split' or 'one'.")
         sys.exit(1)
 
-    try:
-        with open(file_path, "r") as file:
-            text = file.read()
-    except FileNotFoundError:
-        print(f"Error: File '{file_path}' not found.")
-        sys.exit(1)
-    except Exception as e:
-        print(f"Error reading file '{file_path}': {e}")
-        sys.exit(1)
 
     acc_all = get_acc_numbers(text)
 
